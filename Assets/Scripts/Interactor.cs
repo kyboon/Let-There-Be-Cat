eef29bdb09badad5c4353f9a95a41b1ee812f629
Boon <koryuboon@gmail.com>
2021-08-23 09:16:39 +0100

    added cat sprites and animation

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -6,10 +6,11 @@ public class Interactor : MonoBehaviour
 {
 
     List<InteractableProp> interactableProps = new List<InteractableProp>();
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,9 @@ public class Interactor : MonoBehaviour
             {
                 interactableProps[interactableProps.Count - 1].Interact();
             }
+
+            if (animator != null)
+                animator.SetTrigger("Interact");
         }
     }
     public void RegisterInteractable(InteractableProp prop)
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController2D controller;
     private float horizontalMovement = 0f;
     public float horizontalSpeed = 30f;
+    private Animator animator;
 
     private bool jump = false;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,9 +22,18 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal") * horizontalSpeed;
 
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            if (animator != null)
+            {
+                animator.SetTrigger("Jump");
+            }
         }
     }
 
